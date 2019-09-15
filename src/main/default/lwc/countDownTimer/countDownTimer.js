400bf3f441ceb6c82df7cae340f19aa69d3d5718
Anny He <anny.he@salesforce.com>
2019-09-14 19:19:10 -0700

    no more countdown after 0

--- src/main/default/lwc/countDownTimer/countDownTimer.js
+++ src/main/default/lwc/countDownTimer/countDownTimer.js
@@ -3,17 +3,23 @@ import { LightningElement, track } from 'lwc'
 export default class CountDownTimer extends LightningElement {
     // TODO: pass in number of seconds
     @track numberOfSeconds = 6;
+    timerID;
+    
     connectedCallback() {
         this.countDown();
     }
-    countDown() {
-        this.numberOfSeconds -= 1;
+
+    countDown() { 
         this.numberOfSeconds =
-            this.numberOfSeconds > 0 ? this.numberOfSeconds : 0;
-            console.log(this.numberOfSeconds)
-        const self = this;
-        window.setTimeout(function() {
-            self.countDown();
-        }, 1000)
+            this.numberOfSeconds > 0 ? this.numberOfSeconds -= 1 : 0;
+        if (this.numberOfSeconds === 0) {
+            this.dispatchEvent(new CustomEvent('timeout'));
+            clearTimeout(this.timerID);
+        } else {
+            const self = this;
+            this.timerID = window.setTimeout(function() {
+                self.countDown();
+            }, 1000);    
+        }
     }
 }

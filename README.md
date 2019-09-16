b9e8c0d6161b3d814bb342316ff5443bdd2cc2bd
Anny He <anny.he@salesforce.com>
2019-09-16 13:18:42 -0700

    Count down timer
    Added a countdown timer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Quiz App
+
+Hourglass Icon made by [Freepik](https://www.flaticon.com/authors/freepik "Freepik") from [www.flaticon.com](https://www.flaticon.com/ "Flaticon")
---
+++ src/main/default/lwc/countDownTimer/countDownTimer.js
@@ -0,0 +1,22 @@
+import { LightningElement, track } from 'lwc'
+
+export default class CountDownTimer extends LightningElement {
+    // TODO: pass in number of seconds
+    @track numberOfSeconds = 6;
+    timerID;
+    
+    connectedCallback() {
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.timerID = setInterval(() => {
+            this.countDown();
+        }, 1000);   
+    }
+
+    countDown() { 
+        this.numberOfSeconds --;
+        if (this.numberOfSeconds === 0) {
+            this.dispatchEvent(new CustomEvent('timeout'));
+            clearInterval(this.timerID);
+        }
+    }
+}

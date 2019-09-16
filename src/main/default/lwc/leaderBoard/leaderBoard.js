cac9d329e4c400ffdf282af446663b1c24bffac2
Anny He <anny.he@salesforce.com>
2019-09-16 14:07:22 -0700

    Leaderboard component

---
+++ src/main/default/lwc/leaderBoard/leaderBoard.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc'
+
+export default class LeaderBoard extends LightningElement {
+    players = [
+        { name: 'Jerry Wood', score: 315 },
+        { name: 'Brandon Barnes', score: 301 },
+        { name: 'Raymond Knight', score: 292 },
+        { name: 'Trevor McCormick', score: 245 },
+        { name: 'Andrew Fox', score: 203 }
+    ]
+}

cd04a6b21b9acdafa0ce62d794ba2eb1c7385411
pozil <philippe.ozil@gmail.com>
2019-10-23 00:21:07 +0200

    Simplified QR code and made it reusable

--- src/main/default/lwc/qrCode/qrCode.js
+++ src/main/default/lwc/qrCode/qrCode.js
@@ -1,42 +1,15 @@
-import { LightningElement, track, wire } from 'lwc';
-import { reduceErrors } from 'c/errorUtils';
-import getMobileAppUrl from '@salesforce/apex/QuizController.getMobileAppUrl';
+import { LightningElement, api } from 'lwc';
 
 export default class QrCode extends LightningElement {
-    @track error;
-    @track url;
-    @track src;
-    qrCodeInitialized = false;
+    @api size = 150;
+    @api url;
 
-    @wire(getMobileAppUrl)
-    wiredPlayers(result) {
-        const { error, data } = result;
-        if (data) {
-            this.url = data;
-            this.error = undefined;
-        } else if (error) {
-            this.error = reduceErrors(error);
-            this.url = undefined;
+    get imageUrl() {
+        if (!this.url) {
+            return null;
         }
-    }
-
-    renderedCallback() {
-        if (this.qrCodeInitialized || !this.url) {
-            return;
-        }
-        this.qrCodeInitialized = true;
-        const imageUrl =
-            'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=' +
-            encodeURIComponent(this.url);
-
-        fetch(imageUrl)
-            .then(() => {
-                this.src = imageUrl;
-            })
-            .catch(error => {
-                this.error =
-                    'Error from QR code render: ' +
-                    reduceErrors(error).join('.');
-            });
+        return `https://chart.googleapis.com/chart?chs=${this.size}x${
+            this.size
+        }&cht=qr&chl=${encodeURIComponent(this.url)}`;
     }
 }

57073a9d0c3229e9c6dba9c52d73beecb6ad4a3e
pozil <philippe.ozil@gmail.com>
2019-10-30 18:16:35 +0100

    Simplified winner component code

--- src/main/default/lwc/winner/winner.js
+++ src/main/default/lwc/winner/winner.js
@@ -3,23 +3,17 @@ import getWinnerStats from '@salesforce/apex/QuizController.getWinnerStats';
 import { reduceErrors } from 'c/errorUtils';
 
 export default class Winner extends LightningElement {
-    @track winnerPlayer;
+    @track winnerStats;
+    @track error;
+
     @wire(getWinnerStats)
     wiredPlayer({ error, data }) {
         if (data) {
-            this.winnerPlayer =
-                Object.entries(data).length === 0
-                    ? undefined
-                    : (this.winnerPlayer = data);
-
+            this.winnerStats = data;
             this.error = undefined;
         } else if (error) {
             this.error = reduceErrors(error);
-            this.winnerPlayer = undefined;
+            this.winnerStats = undefined;
         }
-    }
-
-    get noWinner() {
-        return this.winnerPlayer === undefined;
     }
 }

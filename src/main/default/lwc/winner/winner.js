2608c4d0bb6461103b6bc266349a207d74922b91
Anny He <anny.he@salesforce.com>
2019-10-29 19:54:33 -0700

    add conditional markup for error, no player joined

--- src/main/default/lwc/winner/winner.js
+++ src/main/default/lwc/winner/winner.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getPlayersSortedByScore from '@salesforce/apex/QuizController.getPlayersSortedByScore';
+import { reduceErrors } from 'c/errorUtils';
 
-export default class Winner extends LightningElement {}
+export default class Winner extends LightningElement {
+    @track winnerPlayer;
+    @wire(getPlayersSortedByScore, { maxFetchCount: 1 })
+    wiredPlayer({ error, data }) {
+        if (data) {
+            this.winnerPlayer = data.length ? data[0] : undefined;
+            this.error = undefined;
+        } else if (error) {
+            this.error = reduceErrors(error);
+            this.winnerPlayer = undefined;
+        }
+    }
+
+    get noWinner() {
+        return this.winnerPlayer === undefined;
+    }
+}

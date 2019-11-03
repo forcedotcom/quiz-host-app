c59bd8f4519b80afe62c2c858df94e11c58d7898
Philippe Ozil <pozil@salesforce.com>
2019-11-03 01:41:25 +0100

    Styled registration page (#35)
    * Styled registration page
    
    * Fixed height to remove vertical scrollbar

--- src/main/default/lwc/registration/registration.js
+++ src/main/default/lwc/registration/registration.js
@@ -2,4 +2,14 @@ import { LightningElement, api } from 'lwc';
 
 export default class Registration extends LightningElement {
     @api quizSettings;
+
+    get shortMobileAppUrl() {
+        if (this.quizSettings) {
+            return this.quizSettings.Player_App_URL_Minified__c.replace(
+                'https://',
+                ''
+            );
+        }
+        return '';
+    }
 }

9242153d96c2e7370fef5387569949eacad2f39a
Philippe Ozil <pozil@salesforce.com>
2019-11-04 17:19:06 +0100

    Dynamic player list at registration (#37)
    * Added dynamic player list at registration
    
    * Improved contrast ratio
    
    * Restyled after scalibility test

---
+++ src/main/default/lwc/playerList/playerList.js
@@ -0,0 +1,51 @@
+import { LightningElement, track } from 'lwc';
+import getPlayersSortedByScore from '@salesforce/apex/QuizController.getPlayersSortedByScore';
+import { reduceErrors } from 'c/errorUtils';
+import * as empApi from 'lightning/empApi';
+
+export default class playerList extends LightningElement {
+    @track error;
+    @track playerNames;
+
+    subscription;
+
+    connectedCallback() {
+        getPlayersSortedByScore()
+            .then(players => {
+                this.playerNames = players.map(player => player.Name);
+                this.error = undefined;
+                this.initEmpApi();
+            })
+            .catch(error => {
+                this.error = reduceErrors(error);
+                this.playerNames = undefined;
+            });
+    }
+
+    initEmpApi() {
+        empApi.onError(error => {
+            // eslint-disable-next-line no-console
+            console.error('Streaming API error: ' + JSON.stringify(error));
+        });
+        empApi
+            .subscribe('/data/Quiz_Player__ChangeEvent', -1, cdcEvent => {
+                this.handlePlayerChangeEvent(cdcEvent);
+            })
+            .then(response => {
+                this.subscription = response;
+            });
+    }
+
+    handlePlayerChangeEvent(cdcEvent) {
+        const { Name } = cdcEvent.data.payload;
+        this.playerNames.push(Name);
+    }
+
+    disconnectedCallback() {
+        if (this.subscription) {
+            empApi.unsubscribe(this.subscription, () => {
+                this.subscription = undefined;
+            });
+        }
+    }
+}

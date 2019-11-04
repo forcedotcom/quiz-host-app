c569d258fabc49c74976804de96c06be070adeb1
pozil <philippe.ozil@gmail.com>
2019-11-04 10:02:05 +0100

    Improved contrast ratio

--- src/main/default/lwc/gameApp/gameApp.js
+++ src/main/default/lwc/gameApp/gameApp.js
@@ -12,8 +12,7 @@ const arrColors = [
     'lightsalmon',
     'mediumaquamarine',
     'mediumseagreen',
-    'mediumturquoise',
-    'darkgoldenrod'
+    'mediumturquoise'
 ];
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));

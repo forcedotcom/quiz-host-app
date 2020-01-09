6c3f1ed563299f938848db1d10e5b9604a2e7c8e
pozil <philippe.ozil@gmail.com>
2020-01-09 10:56:33 +0100

    Bumped project version to 1.0.1

--- src/main/default/lwc/gameApp/gameApp.js
+++ src/main/default/lwc/gameApp/gameApp.js
@@ -14,7 +14,7 @@ export default class GameApp extends LightningElement {
     @track isNextButtonDisabled = true;
     @track currentQuestion;
 
-    HOST_APP_VERSION = '1.0.0';
+    HOST_APP_VERSION = '1.0.1';
 
     @wire(getQuizSettings)
     wiredQuizSettings({ error, data }) {

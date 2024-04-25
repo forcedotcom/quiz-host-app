2367692ea2ba6b37c72bf468dff279e2e7c0485b
Philippe Ozil <pozil@salesforce.com>
2024-04-25 09:43:30 +0200

    fix: QR code generator (#110)

--- src/main/default/lwc/qrCode/qrCode.js
+++ src/main/default/lwc/qrCode/qrCode.js
@@ -8,8 +8,8 @@ export default class QrCode extends LightningElement {
         if (!this.url) {
             return null;
         }
-        return `https://chart.googleapis.com/chart?chs=${this.size}x${
+        return `https://api.qrserver.com/v1/create-qr-code/?size=${this.size}x${
             this.size
-        }&cht=qr&chl=${encodeURIComponent(this.url)}`;
+        }&data=${encodeURIComponent(this.url)}`;
     }
 }

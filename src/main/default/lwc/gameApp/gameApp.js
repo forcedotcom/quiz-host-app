003a2741bd024e38ba4340fc235d6cbd5de3a507
Philippe Ozil <pozil@salesforce.com>
2024-10-07 08:32:28 +0200

    build: bump to API v61.0

--- src/main/default/lwc/gameApp/gameApp.js
+++ src/main/default/lwc/gameApp/gameApp.js
@@ -14,7 +14,7 @@ export default class GameApp extends LightningElement {
     isNextButtonDisabled = true;
     currentQuestion;
 
-    HOST_APP_VERSION = '2.10.0';
+    HOST_APP_VERSION = '2.10.1';
 
     @wire(getQuizSettings)
     wiredQuizSettings({ error, data }) {
